c10aa61093d472085046392662213a84ab805fa5
Sergio Torres <sergiotorresca77@gmail.com>
2022-04-09 18:44:51 -0500

    ajax

--- dataland_site/static/posts/main.js
+++ dataland_site/static/posts/main.js
@@ -1,1 +1,17 @@
 console.log("hola nena");
+
+const helloWorldBox = document.getElementById('hello-world')
+
+
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response){
+        console.log('success', response)
+        helloWorldBox.textContent = response.text
+    },
+    error: function(error){
+        console.log('error', error)
+    }
+})
