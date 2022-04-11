21bb24f06922d754e1b6b381ca874faeb796c3f5
Sergio Torres <sergiotorresca77@gmail.com>
2022-04-10 19:37:55 -0500

    load more post with button

--- dataland_site/static/posts/main.js
+++ dataland_site/static/posts/main.js
@@ -1,56 +1,56 @@
-console.log("hola nena");
+console.log('todo nice')
 
-const helloWorldBox = document.getElementById('hello-world')
 const postsBox = document.getElementById('posts-box')
 const spinnerBox = document.getElementById('spinner-box')
+const loadBtn = document.getElementById('load-btn')
 
-$.ajax({
-    type: 'GET',
-    url: '/hello-world/',
-    success: function(response){
-        console.log('success', response)
-        helloWorldBox.textContent = response.text
-    },
-    error: function(error){
-        console.log('error', error)
-    }
-})
 
-$.ajax({
-    type: 'GET',
-    url: '/data/',
-    success: function(response){
-        console.log(response)
-        const data = response.data
-        //spinner
-        setTimeout(()=>{
-            spinnerBox.classList.add('not-visible')
-            console.log(data)
-            data.forEach(e =>{
-                //bootstrap cards
-                postsBox.innerHTML += `
-                    <div class="card mb-2">
-                        <div class="card-body">
-                            <h5 class="card-title">${e.title}</h5>
-                            <p class="card-text">${e.description}</p>
-                        </div>
-                        <div class="card-footer">
-                            <div class="row">
-                                <div class="col-1">
-                                    <a href="#" class="btn btn-primary">Details</a>
+let visible = 3
+
+const getData =()=>{
+    $.ajax({
+        type: 'GET',
+        url: `/data/${visible}/`,
+        success: function(response){
+            console.log(response)
+            const data = response.data
+            //spinner
+            setTimeout(()=>{
+                spinnerBox.classList.add('not-visible')
+                console.log(data)
+                data.forEach(e =>{
+                    //bootstrap cards
+                    postsBox.innerHTML += `
+                        <div class="card mb-2">
+                            <div class="card-body">
+                                <h5 class="card-title">${e.title}</h5>
+                                <p class="card-text">${e.description}</p>
+                            </div>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-1">
+                                        <a href="#" class="btn btn-primary">Details</a>
+                                    </div>
+                                    <div class="col-1">
+                                        <a href="#" class="btn btn-primary">Like</a>
+                                    </div>
                                 </div>
-                                <div class="col-1">
-                                    <a href="#" class="btn btn-primary">Like</a>
-                                </div>
-
                             </div>
                         </div>
-                    </div>
-                `
-            });
-        },100)
-    },
-    error: function(error){
-        console.log(error)
-    }
+                    `
+                });
+            },100)
+        },
+        error: function(error){
+            console.log(error)
+        }
+    })   
+}
+
+loadBtn.addEventListener('click', ()=>{
+    spinnerBox.classList.remove('not-visible')
+    visible += 3
+    getData()
 })
+
+getData()
